be755ea5ec8203d6221aa310f20691bc7902786c
Chokotrec'h <71605204+Chokotec-h@users.noreply.github.com>
2021-09-19 17:52:34 +0200

    Update Stages.py

--- Stages.py
+++ Stages.py
@@ -5,10 +5,10 @@ class Stage:
     def __init__(self) -> None:
         self.sprite = pygame.image.load("DATA/Images/Stages/Best Stage Ever.png").convert_alpha()
         self.mask = pygame.mask.from_surface(self.sprite)
-        self.rect = self.sprite.get_rect()
+        self.rect = self.sprite.get_rect(topleft=(-100,0))
 
     def draw(self, window, cam):
-        pos = [400, 300]
+        pos = [300, 300]
         pos[0] = pos[0] - cam[0]
         pos[1] = pos[1] - cam[1]
         self.rect.move(self.rect.x - pos[0], self.rect.y - pos[1])
