0ea2fe8a3711329c9e13c0c3cacf30f69a88d219
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2021-10-03 11:40:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
     window = pygame.display.set_mode((width, height))
 
     # Déclaration des variables
-    Char_P1 = Chars.Millet()
+    Char_P1 = Chars.Balan()
     Char_P2 = Chars.Balan2()
     stage = Stages.Stage()
     smoke = list()
