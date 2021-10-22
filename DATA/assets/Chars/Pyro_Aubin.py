b6ed9d314900735b68a9688502e5283e3f62e65f
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2021-10-22 20:53:01 +0200

    Added Konami Code

--- DATA/assets/Chars/Pyro_Aubin.py
+++ DATA/assets/Chars/Pyro_Aubin.py
@@ -4,22 +4,25 @@ from math import pi
 
 ##### Copier
 
-class Nome_Personnage(Char):
+class Pyro_Aubin(Char):
     def __init__(self,x,y) -> None:
         super().__init__(speed=2, dashspeed=3, airspeed=0.9, deceleration=0.7, fallspeed=0.5, fastfallspeed=1, fullhop=13, shorthop=10,
                          doublejumpheight=15,airdodgespeed=6,airdodgetime=3,dodgeduration=15)
 
         self.rect = pygame.Rect(100,0,48,120) # Crée le rectangle de perso
         self.jumpsound = pygame.mixer.Sound("DATA/Musics/jump.wav") # Son test
-        self.name = "Name"
+        self.name = "Pyro-Aubin"
+        self.konamiadd = False
         self.x = x
         self.rect.y = y
+        self.konami = []
     
     def __str__(self) -> str:
-        return "Name"
+        return "Pyro-Aubin"
 
     def special(self): 
-        pass
+        if self.konami == ["Up","Up","Down","Down","Left","Right","Left","Right","B","A"]:
+            self.inputattack("Superspecial")
 
     def animation_attack(self,attack,inputs,stage,other):
         left, right, up, down, fullhop, shorthop, attack_button, special, shield, C_Left, C_Right, C_Up, C_Down, D_Left, D_Right, D_Up, D_Down = inputs # dissociation des inputs
@@ -39,8 +42,16 @@ class Nome_Personnage(Char):
             #    self.active_hitboxes.append(Hitbox(-1.5,88.5,51,48,2*pi/3,18,32,1/150,40,5,self,False))
 
         if attack == "NeutralB":
+            if self.konami == ["Up","Up","Down","Down","Left","Right","Left","Right"] and not self.konamiadd:
+                self.konamiadd = True
+                print("B")
+                self.konami.append("B")
+            elif not self.konamiadd :
+                self.konami = []
+
             if self.frame > 15: # 10 frames de lag
                 self.attack = None
+                self.konamiadd = False
                 self.charge = 0
 
         if attack == "DownB":
@@ -53,6 +64,13 @@ class Nome_Personnage(Char):
                 self.attack = None
 
         if attack == "Jab":
+            if self.konami == ["Up","Up","Down","Down","Left","Right","Left","Right","B"] and not self.konamiadd:
+                self.konamiadd = True
+                print("A")
+                self.konami.append("A")
+                self.attack = None
+            elif not self.konamiadd :
+                self.konami = []
 
             if self.frame > 22: # 10 frames de lag
                 self.attack = None
@@ -112,6 +130,13 @@ class Nome_Personnage(Char):
                     self.lag = self.frame-5 # Auto cancel frame 1-5 et 20+
 
         if attack == "NeutralAir":
+            if self.konami == ["Up","Up","Down","Down","Left","Right","Left","Right","B"] and not self.konamiadd:
+                self.konamiadd = True
+                print("A")
+                self.konami.append("A")
+                self.attack = None
+            elif not self.konamiadd :
+                self.konami = []
 
             if self.frame > 40: # 17 frames de lag
                 self.attack = None
@@ -171,24 +196,66 @@ class Nome_Personnage(Char):
                     self.vx -= self.dashspeed*signe(self.direction)
             if self.frame > 50: # 24 frames de lag
                 self.attack = None
-
+        
         if attack == "UpTaunt":
+            if (self.konami == [] or self.konami == ["Up"]) and not self.konamiadd:
+                print("Up")
+                self.konamiadd = True
+                self.konami.append("Up")
+                self.attack = None
+            elif not self.konamiadd :
+                self.konami = []
             
             if self.frame > 30: # Durée de 30 frames
                 self.attack = None
+                self.konamiadd = False
 
         if attack == "DownTaunt":
+            if (self.konami == ["Up","Up"] or self.konami == ["Up","Up","Down"]) and not self.konamiadd:
+                self.konamiadd = True
+                print("Down")
+                self.konami.append("Down")
+            elif not self.konamiadd :
+                self.konami = []
             
             if self.frame > 30: # Durée de 30 frames
                 self.attack = None
+                self.konamiadd = False
 
         if attack == "LeftTaunt":
+            if (self.konami == ["Up","Up","Down","Down"] or self.konami == ["Up","Up","Down","Down","Left","Right"]) and not self.konamiadd:
+                self.konamiadd = True
+                print("Left")
+                self.konami.append("Left")
+            elif not self.konamiadd :
+                self.konami = []
             
             if self.frame > 30: # Durée de 30 frames
                 self.attack = None
+                self.konamiadd = False
 
         if attack == "RightTaunt":
+            if (self.konami == ["Up","Up","Down","Down","Left"] or self.konami == ["Up","Up","Down","Down","Left","Right","Left"]) and not self.konamiadd:
+                self.konamiadd = True
+                print("Right")
+                self.konami.append("Right")
+            elif not self.konamiadd :
+                self.konami = []
             
+            if self.frame > 30: # Durée de 30 frames
+                self.attack = None
+                self.konamiadd = False
+
+        if (not (D_Up or D_Down or D_Left or D_Right) and self.attack in ("RightTaunt","LeftTaunt","UpTaunt","DownTaunt")
+            or (self.attack == "NeutralB" and attack_button)):
+                self.frame = 150
+
+        if attack == "Superspecial":
+            if self.frame == 8 :
+                print("boum")
+                self.konami = []
+                self.konamiadd = False
+                self.active_hitboxes.append(Hitbox(-140,-100,328,320,pi/4,35,120,1/100,40,10,self,True))
             if self.frame > 30: # Durée de 30 frames
                 self.attack = None
 
