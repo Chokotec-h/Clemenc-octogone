35c94f62b10131a6ec9960ba8dbd00b5aa981935
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2021-11-26 22:57:02 +0100

    added UpB, Down B, Upair, Nair.  Nerfed Uptilt and Downair

--- DATA/assets/Chars/Poissonnier.py
+++ DATA/assets/Chars/Poissonnier.py
@@ -2,7 +2,7 @@
 from DATA.utilities.Animations import get_sprite
 from DATA.utilities.Base_Char import Char, Hitbox, change_left, signe
 import pygame
-from math import pi
+from math import pi, sin
 
 from DATA.utilities.Interface import Texte
 
@@ -10,7 +10,7 @@ from DATA.utilities.Interface import Texte
 
 class Poissonnier(Char):
     def __init__(self,x,y,player) -> None:
-        super().__init__(speed=2, dashspeed=3, airspeed=1.7, deceleration=0.78, fallspeed=1, fastfallspeed=1.8, fullhop=19, shorthop=17,
+        super().__init__(speed=2, dashspeed=3, airspeed=1.7, deceleration=0.78, fallspeed=1, fastfallspeed=1.8, fullhop=19, shorthop=15,
                          doublejumpheight=18,airdodgespeed=6,airdodgetime=3,dodgeduration=15)
 
         self.rect = pygame.Rect(100,0,48,120) # Crée le rectangle de perso
@@ -32,20 +32,27 @@ class Poissonnier(Char):
         left, right, up, down, fullhop, shorthop, attack_button, special, shield, C_Left, C_Right, C_Up, C_Down, D_Left, D_Right, D_Up, D_Down = inputs # dissociation des inputs
         smash = C_Down or C_Left or C_Right or C_Up
         if attack == "UpB":
-            if self.frame == 11: # Saute frame 11
+            if self.frame < 5 :
+                if left : # peut reverse netre les frames 1 et 5
+                    self.look_right = False
+                if right :
+                    self.look_right = True
+            if self.frame < 12 :
+                self.vy -= self.fallspeed
+            if self.frame == 12: # Hitbox frame 12-15
+                self.active_hitboxes.append(Hitbox(-5,90,58,50,-pi/2,20*(self.overheat/200),7,1/150,18*(self.overheat/200),3,self,False))
                 self.can_act = False # ne peut pas agir après un grounded up B
-                self.vy = -20
+                self.vy = -15 - 15*(self.overheat/150)
                 self.attack = None
                 self.doublejump = [True for _ in self.doublejump] # Annule tout les sauts
-            #if self.frame < 6 :
-            #    if left : # peut reverse netre les frames 1 et 5
-            #        self.look_right = False
-            #    if right :
-            #        self.look_right = True
-            #if self.frame == 6: # Hitbox frame 6-11
-            #    self.active_hitboxes.append(Hitbox(-1.5,88.5,51,48,2*pi/3,18,32,1/150,40,5,self,False))
+                self.overheat = max(self.overheat-100,0)
 
         if attack == "NeutralB":
+            if self.frame < 5 :
+                if left :
+                    self.look_right = False
+                if right :
+                    self.look_right = True
             if self.overheat < 200 and special and not (left or right or shield) and self.frame > 12 and self.frame < 16 :
                 self.frame = 13
                 self.overheat += 1
@@ -64,7 +71,7 @@ class Poissonnier(Char):
             if self.overheat > 198 and self.frame == 16:
                 self.projectiles.append(Surchauffe(self.rect.x,self.rect.y,self))
                 self.overheat = 0
-                self.damages += 10
+                self.damages += 20
                 self.lag = 25
 
 
@@ -72,7 +79,9 @@ class Poissonnier(Char):
                 self.attack = None
 
         if attack == "DownB":
-            if self.frame > 20 : # 15 frames de lag
+            if self.frame == 20 :
+                self.projectiles.append(Cerveau(self,other,stage))
+            if self.frame > 40 : # 20 frames de lag
                 self.attack = None
                 self.charge = 0
 
@@ -90,20 +99,20 @@ class Poissonnier(Char):
 
         if attack == "Jab":
             if self.frame == 4 :
-                self.active_hitboxes.append(Hitbox(64,32,64,64,pi/2,1,0.5,0,5,2,self))
-            if self.frame == 7 :
+                self.active_hitboxes.append(Hitbox(64,32,48,48,pi/2,1,0.5,0,5,2,self))
+            if self.frame == 9 :
                 if attack_button :
                     self.frame = 5
-                self.active_hitboxes.append(Hitbox(64,32,64,64,pi/2,1,0.5,0,5,2,self))
+                self.active_hitboxes.append(Hitbox(64,32,48,48,pi/2,1,0.5,0,8,2,self))
             if self.frame == 10 :
-                self.active_hitboxes.append(Hitbox(64,32,100,64,pi/4,4,8,1/200,10,2,self,boum=1))
+                self.active_hitboxes.append(Hitbox(64,32,64,64,pi/4,4,8,1/200,10,2,self,boum=1))
 
             if self.frame > 30: # 20 frames de lag
                 self.attack = None
 
         if attack == "DownTilt":
             if self.frame == 8 :
-                self.active_hitboxes.append(Hitbox(50,100,84,32,pi/5,8,6.2,1/256,9,2,self,boum=5,deflect=True,modifier=0.5))
+                self.active_hitboxes.append(Hitbox(50,100,64,32,pi/5,8,6.2,1/256,9,2,self,boum=5,deflect=True,modifier=0.5))
 
             if self.frame > 20: # 12 frames de lag
                 self.attack = None
@@ -122,20 +131,24 @@ class Poissonnier(Char):
 
         if attack == "UpTilt":
             if self.frame == 10 :
-                self.active_hitboxes.append(Hitbox(50,50,72,32,4*pi/11,14,14.9,1/256,14,2,self,boum=5,deflect=True,modifier=1.3))
+                self.active_hitboxes.append(Hitbox(50,50,48,24,7*pi/11,14,4,1/256,14,2,self,boum=5,deflect=True,modifier=1.3))
             if self.frame == 11 :
-                self.active_hitboxes.append(Hitbox(30,-75,64,64,pi/2,9,9.9,1/256,12,3,self,boum=3,deflect=True,modifier=0.1))
-            if self.frame > 11 and self.frame < 14 :
+                self.active_hitboxes.append(Hitbox(32,-42,24,24,pi/2,9,6,1/256,12,3,self,boum=3,deflect=True,modifier=0.1))
+            if self.frame > 11 and self.frame < 15 :
                 if self.active_hitboxes :
-                    self.active_hitboxes[-1].sizex += 30
+                    self.active_hitboxes[-1].sizex += 24
                     if self.look_right :
-                        self.active_hitboxes[-1].relativex -= 30
-            if self.frame == 14 :
-                self.active_hitboxes.append(Hitbox(change_left(50,72),50,72,32,7*pi/11,14,14.9,1/256,14,2,self,boum=5,deflect=True,modifier=1.3))
+                        self.active_hitboxes[-1].relativex -= 24
+            if self.frame == 15 :
+                self.active_hitboxes.append(Hitbox(change_left(50,72),50,48,24,4*pi/11,14,4,1/256,14,2,self,boum=5,deflect=True,modifier=1.3))
             if self.frame > 25: # 11 Frames de lag
                 self.attack = None
 
         if attack == "UpAir":
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(42,42,64,24,3*pi/4,8,4,1/300,8,2,self,boum=1))
+            if self.frame == 14 :
+                self.active_hitboxes.append(Hitbox(0,-42,48,24,pi/5,14,12,1/200,12,2,self,boum=2))
 
             if self.frame > 25: # 10 frames de lag
                 self.attack = None
@@ -171,12 +184,12 @@ class Poissonnier(Char):
                     self.lag = self.frame-2 # Auto cancel frame 1-2 et 20+
 
         if attack == "DownAir":
-            if self.frame == 10 :
+            if self.frame == 14 :
                 self.active_hitboxes.append(Hitbox(-2,-2,72,12,-pi/3,12,9,1/200,11,8,self))
-            if self.frame > 10 and self.active_hitboxes :
+            if self.frame > 14 and self.active_hitboxes :
                 self.active_hitboxes[-1].relativey += 30
 
-            if self.frame > 25: # 10 frames de lag
+            if self.frame > 30: # 16 frames de lag
                 self.attack = None
 
             if self.grounded :
@@ -185,8 +198,13 @@ class Poissonnier(Char):
                     self.lag = self.frame-5 # Auto cancel frame 1-5 et 20+
 
         if attack == "NeutralAir":
+            if self.frame == 4 :
+                self.active_hitboxes.append(Hitbox(10,32,64,64,pi/5,2,8,0,8,2,self))
+            if self.frame == 7 :
+                self.active_hitboxes.append(Hitbox(10,32,64,64,pi/3,12,12,1/200,14,2,self))
+                self.damages += 0.1
 
-            if self.frame > 40: # 17 frames de lag
+            if self.frame > 20: # 12 frames de lag
                 self.attack = None
 
             if self.grounded :
@@ -326,7 +344,7 @@ class Poissonnier(Char):
 
 class Fireball():
     def __init__(self,x,y,charge,own):
-        self.x = x
+        self.x = x-charge/4
         self.y = y-charge/4
         self.vx = signe(own.direction)*20
         self.sprite = pygame.transform.scale(pygame.image.load("./DATA/Images/Sprites/Projectiles/Fire/1.png"),(round(charge),round(charge)))
@@ -402,7 +420,7 @@ class Surchauffe():
         else :
             self.angle = pi/4
         self.knockback = 29
-        self.damages = 35
+        self.damages = 42
         self.stun = 28
         self.duration = 16
         self.rect = self.sprite.get_rect(topleft=(self.x,self.y))
@@ -420,5 +438,52 @@ class Surchauffe():
         
     def draw(self,window):
         window.blit(self.sprite,(self.x+800,self.y+450))
+
+class Cerveau():
+    def __init__(self,own:Poissonnier,other:Char,stage) -> None:
+        self.sprite = pygame.image.load("./DATA/Images/Sprites/Projectiles/Cerveau.png")
+        self.x = own.x
+        self.y = own.rect.y
+        self.own = own
+        self.other = other
+        self.stage = stage
+        self.vx = -8*signe(own.direction)
+        self.vy = -9
+        self.knockback = 0
+        self.damages = 4
+        self.stun = 0
+        self.duration = 60
+        self.damages_stacking = 0
+        self.rect = self.sprite.get_rect(bottomleft=(self.x,self.y))
+    
+    def update(self):
+        self.duration -= 1
+        self.y += self.vy
+        self.x += self.vx
+        self.rect = self.sprite.get_rect(topleft=(self.x,self.y))
+        if self.rect.colliderect(self.stage.mainplat.rect):
+            if self.y < self.stage.mainplat.y :
+                self.vy = 0
+                self.vx = 0
+            else :
+                self.vx = -self.vx
+        else :
+            self.vy += 1
+        
+        if self.rect.colliderect(self.other.rect):
+            if not (self.other.lag or self.other.hitstun) :
+                self.other.combo = 0
+                self.other.combodamages = 0
+            self.other.vx = 2*signe(self.other.direction)
+            self.other.vy = -4
+            self.other.hitstun = 30
+    
+    def deflect(self):
+        self.vx = -self.vx
+        self.own,self.other = self.other,self.own
+
+    def draw(self,window):
+        window.blit(self.sprite, (self.rect.x+800,self.rect.y+450)) # on dessine le sprite
+        
 
 ##### Autres skins
