c91206c373952feb27535429ecb16d035cd96881
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2021-12-03 20:43:49 +0100

    Added Aerials. Need to correct Nair

--- DATA/assets/Chars/Isaac.py
+++ DATA/assets/Chars/Isaac.py
@@ -1,6 +1,6 @@
-from DATA.utilities.Base_Char import Char, Hitbox, signe
+from DATA.utilities.Base_Char import Char, Hitbox, change_left, signe
 import pygame
-from math import pi
+from math import pi, sqrt
 
 ##### Isaac Newton
 
@@ -15,6 +15,9 @@ class Isaac(Char):
         self.x = x
         self.rect.y = y
         self.player = player
+        self.antigravity = False
+        self.otherfallspeed = 0
+        self.otherfastfallspeed = 0
     
     def __str__(self) -> str:
         return "Isaac"
@@ -22,7 +25,7 @@ class Isaac(Char):
     def special(self):
         pass
 
-    def animation_attack(self,attack,inputs,stage,other):
+    def animation_attack(self,attack,inputs,stage,other:Char):
         left, right, up, down, fullhop, shorthop, attack_button, special, shield, C_Left, C_Right, C_Up, C_Down, D_Left, D_Right, D_Up, D_Down = inputs # dissociation des inputs
         smash = C_Down or C_Left or C_Right or C_Up
         if attack == "UpB":
@@ -35,7 +38,7 @@ class Isaac(Char):
                 self.attack = None
                 self.can_act = True
                 self.upB = False
-                self.vy = self.fallspeed
+                self.vy *= 0.75
             #if self.frame < 6 :
             #    if left : # peut reverse netre les frames 1 et 5
             #        self.look_right = False
@@ -88,46 +91,159 @@ class Isaac(Char):
                 self.attack = None
 
         if attack == "UpAir":
+            if self.frame == 0 :
+                self.antigravity = False
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(48,48,36,4,pi/2,14,8,1/200,18,7,self))
+            if self.frame > 10 and self.frame < 17 :
+                if self.active_hitboxes:
+                    self.active_hitboxes[-1].sizey += 10
+                    self.active_hitboxes[-1].relativey -= 10
+                elif not self.antigravity :
+                    self.otherfallspeed = other.fallspeed
+                    self.otherfastfallspeed = other.fastfallspeed
+                    self.antigravity = True
+                
+            if self.frame > 1 and self.frame < 25 and self.antigravity :
+                other.fallspeed = 0
+                print("Upair")
+                other.fastfallspeed = 0
 
-            if self.frame > 25: # 10 frames de lag
+            if self.frame > 25 and self.antigravity: # 8 frames de lag
+                print("Reset",self.otherfastfallspeed)
+                other.fallspeed = self.otherfallspeed
+                other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
 
             if self.grounded :
+                if self.antigravity :
+                    print("Reset",self.otherfastfallspeed)
+                    other.fallspeed = self.otherfallspeed
+                    other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
-                if self.frame < 15 :
-                    self.lag = self.frame-2 # Auto cancel frame 1-2 et 15+
+                if self.frame < 40 :
+                    self.lag = self.frame-3 # Auto cancel frame 1-3 et 40+
 
         if attack == "ForwardAir":
+            if self.frame == 1 :
+                self.antigravity = False
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(48,48,4,36,0,14,8,1/200,18,7,self))
+            if self.frame > 10 and self.frame < 17 :
+                if self.active_hitboxes:
+                    self.active_hitboxes[-1].sizex += 10
+                    if not self.look_right :
+                        self.active_hitboxes[-1].relativex -= 10
+                elif not self.antigravity :
+                    self.otherfallspeed = other.fallspeed
+                    self.otherfastfallspeed = other.fastfallspeed
+                    self.antigravity = True
+                
+            if self.frame > 1 and self.frame < 25 and self.antigravity :
+                print("ForwardAir")
+                other.fallspeed = 0
+                other.fastfallspeed = 0
 
-            if self.frame > 50: # 29 frames de lag
+            if self.frame > 25 and self.antigravity: # 8 frames de lag
+                print("Reset",self.otherfastfallspeed)
+                other.fallspeed = self.otherfallspeed
+                other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
 
             if self.grounded :
+                if self.antigravity :
+                    print("Reset",self.otherfastfallspeed)
+                    other.fallspeed = self.otherfallspeed
+                    other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
                 if self.frame < 40 :
                     self.lag = self.frame-3 # Auto cancel frame 1-3 et 40+
 
         if attack == "BackAir":
+            if self.frame == 1 :
+                self.gravity_hit = False
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(change_left(48,64)+60,48,4,36,pi,14,8,1/200,18,7,self))
+            if self.frame > 10 and self.frame < 17 :
+                if self.active_hitboxes:
+                    self.active_hitboxes[-1].sizex += 10
+                    if self.look_right :
+                        self.active_hitboxes[-1].relativex -= 10
+                elif not self.antigravity :
+                    self.otherfallspeed = other.fallspeed
+                    self.otherfastfallspeed = other.fastfallspeed
+                    self.antigravity = True
+                
+            if self.frame > 1 and self.frame < 25 and self.antigravity :
+                print("BackAir")
+                other.fallspeed = 0
+                other.fastfallspeed = 0
 
-            if self.frame > 25: # 14 frames de lag
+            if self.frame > 25 and self.antigravity: # 8 frames de lag
+                print("Reset",self.otherfastfallspeed)
+                other.fallspeed = self.otherfallspeed
+                other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
 
             if self.grounded :
+                if self.antigravity :
+                    print("Reset",self.otherfastfallspeed)
+                    other.fallspeed = self.otherfallspeed
+                    other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
-                if self.frame < 20 :
-                    self.lag = self.frame-2 # Auto cancel frame 1-2 et 20+
+                if self.frame < 40 :
+                    self.lag = self.frame-3 # Auto cancel frame 1-3 et 40+
 
         if attack == "DownAir":
+            if self.frame == 1 :
+                self.antigravity = False
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(48,48,36,4,-pi/2,14,8,1/200,18,7,self))
+            if self.frame > 10 and self.frame < 17 :
+                if self.active_hitboxes:
+                    self.active_hitboxes[-1].sizey += 10
+                elif not self.antigravity :
+                    self.otherfallspeed = other.fallspeed
+                    self.otherfastfallspeed = other.fastfallspeed
+                    self.antigravity = True
+                
+            if self.frame > 10 and self.frame < 25 and self.antigravity :
+                print("DownAir")
+                other.fallspeed = 0
+                other.fastfallspeed = 0
 
-            if self.frame > 25: # 10 frames de lag
+            if self.frame > 25 and self.antigravity: # 8 frames de lag
+                print("Reset",self.otherfastfallspeed)
+                other.fallspeed = self.otherfallspeed
+                other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
 
             if self.grounded :
+                if self.antigravity :
+                    print("Reset",self.otherfastfallspeed)
+                    other.fallspeed = self.otherfallspeed
+                    other.fastfallspeed = self.otherfastfallspeed
                 self.attack = None
-                if self.frame < 20 :
-                    self.lag = self.frame-5 # Auto cancel frame 1-5 et 20+
+                if self.frame < 40 :
+                    self.lag = self.frame-3 # Auto cancel frame 1-3 et 40+
 
         if attack == "NeutralAir":
+            if self.frame > 4 and self.frame < 21 : # Windbox frame 5-20
+                self.active_hitboxes.append(Hitbox(-216,-180,480,480,0,0,0,0,0,2,self,boum=-20))
+                if self.active_hitboxes[-1].hit.colliderect(other.rect):
+                    force = min(6.67*2*200/max(((sqrt(self.rect.x**2+self.rect.y**2))-sqrt(other.rect.x**2+other.rect.y**2))**2,0.1),12)
+                    if (sqrt(self.rect.x**2+self.rect.y**2))-sqrt(other.rect.x**2+other.rect.y**2) == 0 :
+                        fx = 0
+                        fy = 0
+                    else :
+                        fx = min(max((self.rect.x-other.rect.x)/abs(((sqrt(self.rect.x**2+self.rect.y**2))-sqrt(other.rect.x**2+other.rect.y**2))),-1),1)
+                        fy = min(max((self.rect.y - other.rect.y)/abs(((sqrt(self.x**2+self.rect.y**2))-sqrt(other.x**2+other.rect.y**2))),-1),1)
+
+                    other.vx += force * fx
+                    other.vy += force * fy
+                    print(other.vx,other.vy)
+            if self.frame == 10 :
+                self.active_hitboxes.append(Hitbox(-5,-5,58,130,pi/4,16,8,1/200,15,10,self))
 
             if self.frame > 40: # 17 frames de lag
                 self.attack = None
