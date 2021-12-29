f4851f76bf6efe51b5c467dae050757dab5186dc
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2021-12-29 12:48:36 +0100

    Added Settings.txt

--- DATA/utilities/Sound_manager.py
+++ DATA/utilities/Sound_manager.py
@@ -1,7 +1,10 @@
 from pygame.mixer import Sound
 
-musicvolume = 1
-soundvolume = 0.7
+with open("DATA/utilities/Settings.txt","r") as settings :
+    r = settings.read()
+    r = r.split("=")
+    musicvolume = float(r[1].split("\n")[0])
+    soundvolume = float(r[2].split("\n")[0])
 
 def playsound(sound):
     play = Sound(sound)
