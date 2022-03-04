e9cf847b513437cb46bf7fe4127ab2cda243ff8d
ChOc0-NV <71605204+Chokotec-h@users.noreply.github.com>
2022-03-04 22:03:23 +0100

    reorganized sprites ; added Renault icon

--- DATA/assets/animations.py
+++ DATA/assets/animations.py
@@ -4,7 +4,7 @@ import pygame
 
 icons ={
     "Balan":pygame.image.load("./DATA/Images/Sprites/Chars/Balan/icon.png"),
-    "Joueur de air-president":pygame.image.load("./DATA/Images/Sprites/Chars/Joueur_de_Air_President/icon.png"), "Spamton":pygame.image.load("./DATA/Images/Sprites/Chars/Joueur_de_Air_President/Spamton/icon.png"),
+    "Joueur de air-president":pygame.image.load("./DATA/Images/Sprites/Chars/Joueur_de_Air_President/icon.png"), "Spamton":pygame.image.load("./DATA/Images/Sprites/Chars/Joueur_de_Air_President/icon_S.png"),
     "Millet":pygame.image.load("./DATA/Images/Sprites/Chars/None/icon.png"),
     "Gregoire":pygame.image.load("./DATA/Images/Sprites/Chars/Gregoire/icon.png"),
     "Reignaud":pygame.image.load("./DATA/Images/Sprites/Chars/None/icon.png"),
@@ -12,12 +12,12 @@ icons ={
     "Pyro-Aubin":pygame.image.load("./DATA/Images/Sprites/Chars/Pyro_Aubin/icon.png"),
     "Kebab":pygame.image.load("./DATA/Images/Sprites/Chars/Kebab/icon.png"),
     "Poissonnier":pygame.image.load("./DATA/Images/Sprites/Chars/None/icon.png"),
-    "Renault":pygame.image.load("./DATA/Images/Sprites/Chars/None/icon.png"),
+    "Renault":pygame.image.load("./DATA/Images/Sprites/Chars/Renault/icon.png"),
     "Isaac":pygame.image.load("./DATA/Images/Sprites/Chars/None/icon.png"),
 }
 icons64 ={
     "Balan":pygame.transform.scale(icons["Balan"],(64,64)),
-    "Joueur de air-president":pygame.transform.scale(icons["Joueur de air-president"],(64,64)), "Spamton":pygame.image.load("./DATA/Images/Sprites/Chars/Joueur_de_Air_President/Spamton/icon.png"),
+    "Joueur de air-president":pygame.transform.scale(icons["Joueur de air-president"],(64,64)), "Spamton":pygame.transform.scale(icons["Spamton"],(64,64)),
     "Millet":pygame.transform.scale(icons["Millet"],(64,64)),
     "Gregoire":pygame.transform.scale(icons["Gregoire"],(64,64)),
     "Reignaud":pygame.transform.scale(icons["Reignaud"],(64,64)),
@@ -38,6 +38,16 @@ Nonesprite = {
     "jump":("./DATA/Images/Sprites/Chars/None/Nameless_sprite.png",((0,0,12,30),),1,True),
     "fall":("./DATA/Images/Sprites/Chars/None/Nameless_sprite.png",((0,0,12,30),),1,True),
     "airdodge":("./DATA/Images/Sprites/Chars/None/Nameless_sprite.png",((0,0,12,30),),1,True),
+
+}
+
+Renault = {
+    "idle":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
+    "walk":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
+    "run":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
+    "jump":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
+    "fall":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
+    "airdodge":("./DATA/Images/Sprites/Chars/Renault/Default/idle.png",((2,1,16,35),),1,True),
 
 }
 
@@ -82,32 +92,32 @@ Spamton = {
 }
 
 Gregoire = {
-    "idle":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_idle.png",((1,0,17,36),(18,1,18,35),(36,0,17,36),(55,1,16,35)),8,True),
-    "walk":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_walk.png",((1,0,16,36),(18,0,16,36),(34,0,20,36),(54,0,18,36),(72,0,16,36),(88,0,16,36),(106,0,16,36)),8,True),
-    "run":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_run.png",((1,0,16,36),(21,0,20,36),(50,0,18,36),(73,0,20,36),(96,0,24,36),(128,0,18,36)),12,True),
-    "jump":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_jump.png",((3,2,17,34),(25,1,15,35),(46,0,16,37),(63,1,21,36)),12,False),
-    "fall":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_jump.png",((63,1,21,36),),8,True),
-    "airdodge":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_parry_airdodge.png",((1,0,15,36),(20,0,17,36),(39,0,17,36)),8,False),
-    "jab":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_jab.png",((1,0,17,36),(19,0,19,36),(38,0,19,36),(60,0,19,35),(79,0,17,36),(97,0,18,36),(120,0,16,36),(136,0,18,36)),20,True),
-    "nair":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_nair.png",((0,0,19,36),(20,0,14,36),(37,0,15,36),(53,0,15,35),(70,0,21,36),(91,0,18,36)),12,False),
-    "dashattack":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dashattack.png",((1,2,18,36),(21,4,18,34),(42,9,25,29),(69,20,18,18),(89,20,18,18),(110,20,18,18),(130,20,18,18),(150,20,18,18),(170,20,18,18),(191,20,18,18),(211,13,29,25)),20,False),
-    "downb_charge":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_downBCharge.png",((1,0,29,36),(30,0,32,36),(63,0,26,36)),10,False),
-    "downb_release":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_downBRelease.png",((0,0,34,36),(37,0,32,36),(69,0,36,36),(106,0,34,36),(146,0,34,36)),10,False),
-    "dsmash":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dsmash.png",((3,0,20,36),(28,0,17,36),(48,0,17,36),(65,0,18,36),(83,0,18,36)),9,False),
-    "dtilt":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dtilt.png",((2,1,21,35),(26,3,17,33),(43,3,16,33),(60,6,17,30),(80,3,14,33)),10,False),
-    "fair":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_fair.png",((2,0,14,36),(21,0,14,36),(40,0,14,36),(62,0,14,36),(79,0,22,36),(106,0,30,36),(136,0,24,36),(160,0,16,36),(180,0,14,36)),20,False),
-    "fair2":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_fair2.png",((0,0,14,36),(15,0,37,36)),0.75,False),
-    "fsmash":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_fsmash.png",((2,0,21,36),(27,0,20,36),(50,0,17,36),(75,0,16,36),(101,0,23,36),(126,0,23,36),(153,0,23,36),(179,0,23,36),(209,0,23,36),(232,0,17,36)),10,False),
-    "ftilt":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_ftilt.png",((2,0,21,36),(31,0,14,36),(57,0,19,36),(84,0,19,36)),10,False),
-    "sideB":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_la_mort.png",((1,0,20,36),(24,0,28,36),(54,0,28,36),(85,0,28,36),(117,0,21,36)),10,False),
-    "neutralB":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_neutralB.png",((4,0,16,36),(23,0,17,36),(44,0,18,36),(64,0,17,36),(82,0,16,36)),10,False),
-    "uair":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_uair.png",((2,0,21,36),(27,0,16,36),(44,0,15,36),(60,0,15,36)),10,False),
-    "usmash":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_usmash.png",((1,2,18,36),(21,4,18,34),(39,4,21,34),(60,4,18,34),(78,1,14,37)),10,False),
-    "utilt":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_utilt.png",((1,6,27,30),(29,0,17,36),(54,5,25,31),(83,0,14,36)),10,False),
-    "dair_dive":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dair_dive.png",((4,6,25,31),(31,8,25,29),(63,12,29,25),(97,0,14,37)),10,False),
-    "dair_fall":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dair_fall.png",((6,0,14,37),(25,12,30,25),(61,7,25,29),(93,9,29,25),(125,2,21,36)),15,False),
-    "dair_ground":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_dair_ground.png",((6,0,14,37),(25,12,29,25),(61,8,25,29),(94,4,18,34)),10,False),
-    "bair":("./DATA/Images/Sprites/Chars/Gregoire/gregoire_bair.png",((2,0,21,36),(25,0,22,36),(48,0,23,36),(78,0,14,36),(99,0,21,36),(122,0,26,36),(148,0,16,36)),10,False),
+    "idle":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_idle.png",((1,0,17,36),(18,1,18,35),(36,0,17,36),(55,1,16,35)),8,True),
+    "walk":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_walk.png",((1,0,16,36),(18,0,16,36),(34,0,20,36),(54,0,18,36),(72,0,16,36),(88,0,16,36),(106,0,16,36)),8,True),
+    "run":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_run.png",((1,0,16,36),(21,0,20,36),(50,0,18,36),(73,0,20,36),(96,0,24,36),(128,0,18,36)),12,True),
+    "jump":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_jump.png",((3,2,17,34),(25,1,15,35),(46,0,16,37),(63,1,21,36)),12,False),
+    "fall":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_jump.png",((63,1,21,36),),8,True),
+    "airdodge":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_parry_airdodge.png",((1,0,15,36),(20,0,17,36),(39,0,17,36)),8,False),
+    "jab":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_jab.png",((1,0,17,36),(19,0,19,36),(38,0,19,36),(60,0,19,35),(79,0,17,36),(97,0,18,36),(120,0,16,36),(136,0,18,36)),20,True),
+    "nair":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_nair.png",((0,0,19,36),(20,0,14,36),(37,0,15,36),(53,0,15,35),(70,0,21,36),(91,0,18,36)),12,False),
+    "dashattack":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dashattack.png",((1,2,18,36),(21,4,18,34),(42,9,25,29),(69,20,18,18),(89,20,18,18),(110,20,18,18),(130,20,18,18),(150,20,18,18),(170,20,18,18),(191,20,18,18),(211,13,29,25)),20,False),
+    "downb_charge":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_downBCharge.png",((1,0,29,36),(30,0,32,36),(63,0,26,36)),10,False),
+    "downb_release":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_downBRelease.png",((0,0,34,36),(37,0,32,36),(69,0,36,36),(106,0,34,36),(146,0,34,36)),10,False),
+    "dsmash":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dsmash.png",((3,0,20,36),(28,0,17,36),(48,0,17,36),(65,0,18,36),(83,0,18,36)),9,False),
+    "dtilt":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dtilt.png",((2,1,21,35),(26,3,17,33),(43,3,16,33),(60,6,17,30),(80,3,14,33)),10,False),
+    "fair":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_fair.png",((2,0,14,36),(21,0,14,36),(40,0,14,36),(62,0,14,36),(79,0,22,36),(106,0,30,36),(136,0,24,36),(160,0,16,36),(180,0,14,36)),20,False),
+    "fair2":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_fair2.png",((0,0,14,36),(15,0,37,36)),0.75,False),
+    "fsmash":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_fsmash.png",((2,0,21,36),(27,0,20,36),(50,0,17,36),(75,0,16,36),(101,0,23,36),(126,0,23,36),(153,0,23,36),(179,0,23,36),(209,0,23,36),(232,0,17,36)),10,False),
+    "ftilt":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_ftilt.png",((2,0,21,36),(31,0,14,36),(57,0,19,36),(84,0,19,36)),10,False),
+    "sideB":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_la_mort.png",((1,0,20,36),(24,0,28,36),(54,0,28,36),(85,0,28,36),(117,0,21,36)),10,False),
+    "neutralB":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_neutralB.png",((4,0,16,36),(23,0,17,36),(44,0,18,36),(64,0,17,36),(82,0,16,36)),10,False),
+    "uair":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_uair.png",((2,0,21,36),(27,0,16,36),(44,0,15,36),(60,0,15,36)),10,False),
+    "usmash":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_usmash.png",((1,2,18,36),(21,4,18,34),(39,4,21,34),(60,4,18,34),(78,1,14,37)),10,False),
+    "utilt":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_utilt.png",((1,6,27,30),(29,0,17,36),(54,5,25,31),(83,0,14,36)),10,False),
+    "dair_dive":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dair_dive.png",((4,6,25,31),(31,8,25,29),(63,12,29,25),(97,0,14,37)),10,False),
+    "dair_fall":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dair_fall.png",((6,0,14,37),(25,12,30,25),(61,7,25,29),(93,9,29,25),(125,2,21,36)),15,False),
+    "dair_ground":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_dair_ground.png",((6,0,14,37),(25,12,29,25),(61,8,25,29),(94,4,18,34)),10,False),
+    "bair":("./DATA/Images/Sprites/Chars/Gregoire/Default/gregoire_bair.png",((2,0,21,36),(25,0,22,36),(48,0,23,36),(78,0,14,36),(99,0,21,36),(122,0,26,36),(148,0,16,36)),10,False),
 }
 
 Pyro_Aubin = {
@@ -168,7 +178,7 @@ animations = {
     "Pyro-Aubin":Pyro_Aubin,
     "Kebab":Kebab,
     "Poissonnier":Nonesprite,
-    "Renault":Nonesprite,
+    "Renault":Renault,
     "Isaac":Nonesprite,
     "Training":Training
 }
