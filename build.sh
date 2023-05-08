2b7834f207f2545a9c385622a8691c70fe3e924f
idimension <41388029+idimension18@users.noreply.github.com>
2023-05-08 21:52:56 +0200

    change build.sh to Makefile

--- build.sh
+++
@@ -1,5 +0,0 @@
-pyinstaller --onefile --windowed --icon="DATA/Images/logo.ico" main.py
-pyinstaller main.spec
-mkdir "dist/Clémenc'octogonne"
-mv dist/* "dist/Clémenc'octogonne"
-cp -r DATA "dist/Clémenc'octogonne"
