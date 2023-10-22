37fbee9d149e310f29182e51b75804341130ef89
Nicolas VINCENT <nicolas.vincent1@ens-rennes.fr>
2023-10-22 22:36:29 +0200

    clarified inputs

--- DATA/utilities/IA/IA.py
+++ DATA/utilities/IA/IA.py
@@ -3,6 +3,29 @@ class IA():
         self.char = char
 
     def inputs(self,game):
-        # inputs : left, right, up, down, fullhop, shorthop, attack, special, shield, C_Left, C_Right, C_Up, C_Down, D_Left, D_Right, D_Up, D_Down, ? , L_Tilt, R_Tilt, U_Tilt, D_Tilt, 
+        """ inputs :
+            0 : left
+            1 : right
+            2 : up
+            3 : down
+            4 : fullhop
+            5 : shorthop
+            6 : attack
+            7 : special
+            8 : shield (hold to run)
+            9 : C_Left
+            10 : C_Right
+            11 : C_Up
+            12 : C_Down
+            13 : D_Left
+            14 : D_Right
+            15 : D_Up
+            16 : D_Down
+            17 : 2e shorthop
+            18 : L_Tilt
+            19 : R_Tilt
+            20 : U_Tilt
+            21 : D_Tilt
+        """
         inputs = [False for _ in range(23)]
         return inputs
